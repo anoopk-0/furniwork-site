b94ca466e4841abfe89e5db9daaf0ad7d06a066c
anoopk-0 <anp.k072@gmail.com>
2024-02-13 12:21:12 +0530

    change test

--- src/layouts/navbar/Navbar.test.js
+++ src/layouts/navbar/Navbar.test.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import NavBar from "./NavBar";
 
 describe("NavBar component", () => {
   it("renders without crashing", () => {
     render(<NavBar />);
-  });
-
-  it("displays menu items", () => {
-    const { getByText } = render(<NavBar />);
-    const productsMenuItem = getByText("Products");
-    expect(productsMenuItem).toBeInTheDocument();
   });
 });
